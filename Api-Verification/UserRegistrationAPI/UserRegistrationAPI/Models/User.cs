2ee15cd4969670ba9da5092f010b2b762fc257ed
Dallacqua90125 <156970429+Dallacqua90125@users.noreply.github.com>
2025-02-24 12:50:16 -0300

    Primeiro commit

---
+++ Api-Verification/UserRegistrationAPI/UserRegistrationAPI/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserRegistrationAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public bool IsEmailVerified { get; set; }
+
+        public string EmailVerificationCode { get; set; }
+    }
+}
---
+++ Api-Verification/UserRegistrationAPI/UserRegistrationAPI/Services/EmailService.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace UserRegistrationAPI.Services
+{
+
+    public class EmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void SendEmail(string to, string subject, string body)
+        {
+            var emailSettings = _configuration.GetSection("EmailSettings");
+            var smtpClient = new SmtpClient(emailSettings["SmtpServer"])
+            {
+                Port = int.Parse(emailSettings["SmtpPort"]),
+                Credentials = new NetworkCredential(emailSettings["SmtpUsername"], emailSettings["SmtpPassword"]),
+                EnableSsl = true,
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(emailSettings["FromEmail"]),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true,
+            };
+            mailMessage.To.Add(to);
+
+            smtpClient.Send(mailMessage);
+        }
+    }
+}
